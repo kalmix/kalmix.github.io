19973d1426086792a136c6d21d430c6cf95fd470
Kalmix <87293493+kalmix@users.noreply.github.com>
2021-08-31 17:02:31 -0400

    Add files via upload

---
+++ mixser/assets/mbr-popup-btns/mbr-popup-btns.js
@@ -0,0 +1,1 @@
+var isBuilder=$("html").hasClass("is-builder");isBuilder||"undefined"!==typeof window.initPopupBtnPlugin||(window.initPopupBtnPlugin=!0,$("section.popup-btn-cards .card-wrapper").each(function(a,b){$(this).addClass("popup-btn")}));
