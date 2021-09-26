38dac68d0a4706c876f868181e2714dc2536e59a
Jasiel <51069301+Jasielx1@users.noreply.github.com>
2021-09-25 20:41:57 -0400

    update

--- kagency/preloader.js
+++ kagency/preloader.js
@@ -1,12 +1,1 @@
-//declare body class and add preloader-site class
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-//when content is loaded -> delete preloader-site and fadeOut preloader
-$(window).load(function() {
-		//.fadeOut("number" ms -> change duration)
-		//.fadeOut("slow") -> slow fade out
-    $('.preloader-wrapper').fadeOut();
-    $('body').removeClass('preloader-site');
-});
+$(document).ready(function(a){var b=a("body");b.addClass("preloader-site")});$(window).load(function(){$(".preloader-wrapper").fadeOut();$("body").removeClass("preloader-site")});
